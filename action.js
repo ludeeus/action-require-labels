ed5effe2fe9ff4efa1fe4f569dd57fa34961150b
ludeeus <joasoe@gmail.com>
2023-06-29 15:38:52 +0000

    Check labels

--- action.js
+++ action.js
@@ -2,7 +2,16 @@ const fs = require("fs");
 
 function runAction() {
     const eventData = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH))
-    console.log(eventData)
+    if (!eventData.pull_request) {
+        throw new Error("This is not a pull request.")
+    }
+
+    if (!eventData.pull_request.labels || eventData.pull_request.labels.lenght === 0) {
+        throw new Error("No labels defined on the pull request.")
+    }
+
+    const prLabels = eventData.pull_request.labels.map(label => label.name)
+    console.log(prLabels)
 }
 
 
