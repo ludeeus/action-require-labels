bc89e16b6dd72e2106cca9570c6151fb3a2b2205
ludeeus <joasoe@gmail.com>
2023-06-29 15:35:23 +0000

    parse it

--- action.js
+++ action.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 
 function runAction() {
-    const eventData = fs.readFileSync(process.env.GITHUB_EVENT_PATH)
-    console.log(eventData.pull_request)
+    const eventData = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH))
+    console.log(eventData)
 }
 
 
