70c6ab13a8abe39721d4b87417536b457959a9f0
ludeeus <joasoe@gmail.com>
2023-06-29 15:46:26 +0000

    test

--- action.js
+++ action.js
@@ -1,12 +1,30 @@
 const fs = require("fs");
 
 function runAction() {
-    const eventData = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH))
+    let eventData;
+    const eventPath = process.env.GITHUB_EVENT_PATH
+    const actionPath = process.env.GITHUB_ACTION_PATH
+
+    if (eventPath && fs.existsSync(eventPath)) {
+        eventData = JSON.parse(fs.readFileSync(eventPath, {encoding: 'utf8'}))
+    } else {
+        throw new Error(`GITHUB_EVENT_PATH ${eventPath} does not exist`)
+    }
+
+    if (actionPath && fs.existsSync(actionPath)) {
+        fs.readdirSync(actionPath).forEach(file => {
+            console.log(file);
+          });
+    } else {
+        throw new Error(`GITHUB_ACTION_PATH ${actionPath} does not exist`)
+    }
+      
+
     if (!eventData.pull_request) {
         throw new Error("This is not a pull request.")
     }
 
-    if (!eventData.pull_request.labels || eventData.pull_request.labels.lenght === 0) {
+    if (!eventData.pull_request.labels || eventData.pull_request.labels.length === 0) {
         throw new Error("No labels defined on the pull request.")
     }
 
