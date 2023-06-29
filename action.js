b1963b5a7d238303f38c71c8057a29971f461dd4
ludeeus <joasoe@gmail.com>
2023-06-29 15:49:23 +0000

    test

--- action.js
+++ action.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 function runAction() {
     let eventData;
     const eventPath = process.env.GITHUB_EVENT_PATH
-    const actionPath = process.env.GITHUB_ACTION_PATH
 
     if (eventPath && fs.existsSync(eventPath)) {
         eventData = JSON.parse(fs.readFileSync(eventPath, {encoding: 'utf8'}))
@@ -11,12 +10,8 @@ function runAction() {
         throw new Error(`GITHUB_EVENT_PATH ${eventPath} does not exist`)
     }
 
-    if (actionPath && fs.existsSync(actionPath)) {
-        fs.readdirSync(actionPath).forEach(file => {
-            console.log(file);
-          });
-    } else {
-        throw new Error(`GITHUB_ACTION_PATH ${actionPath} does not exist`)
+    for (const key in process.env.keys()) {
+        console.log(key)
     }
       
 
