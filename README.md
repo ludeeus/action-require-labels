3dd532f2109324d56a81edb1a0dc6bb459aedbea
Joakim Sørensen <joasoe@gmail.com>
2023-06-29 18:10:53 +0200

    Add content to the action (#2)
    * Update
    
    * parse it
    
    * Check labels
    
    * test
    
    * test
    
    * test
    
    * fix
    
    * update
    
    * display matching
    
    * final

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # action-require-labels
+
+This action privides a simple solution to require spesific labels on pull requests. Without the need to use the GitHub API and tokens.
+
+## Inputs
+
+### `labels`
+
+**Required** Comma seperated string of labels to look for.
+
+## Example usage
+
+```yaml
+uses: ludeeus/action-require-labels@main
+with:
+ labels: >-
+    bugfix, breaking-change, new-feature
+```
--- action.js
+++ action.js
@@ -1,12 +1,41 @@
 const fs = require("fs");
 
 function runAction() {
-    console.log(process.env.GITHUB_EVENT_PATH)
-    const eventData = fs.readFileSync('student.json')
+    const eventPath = process.env.GITHUB_EVENT_PATH
+
+    if (!eventPath || !fs.existsSync(eventPath)) {
+        throw new Error(`GITHUB_EVENT_PATH ${eventPath} does not exist`)
+    }
+
+    const eventData = JSON.parse(fs.readFileSync(eventPath, {encoding: 'utf8'}))
+
+    if (!eventData.pull_request) {
+        throw new Error("This is not a pull request.")
+    }
+
+    if (!eventData.pull_request.labels || eventData.pull_request.labels.length === 0) {
+        throw new Error("No labels defined on the pull request.")
+    }
+
+    const inputLabels = process.env.INPUT_LABELS
+    
+    if (!inputLabels) {
+        throw new Error("No required labels defined for the action.")
+    }
+
+    const requiredLabels = new Set(inputLabels.split(",").map(label => label.trim()))
+    const prLabels = eventData.pull_request.labels.map(label => label.name)
+
+    console.log(`Required labels ${requiredLabels}`)
+    console.log(`Pull request labels ${prLabels}`)
+
+    const matchingLabels = prLabels.filter(label => requiredLabels.has(label))
+    console.log(`Found ${matchingLabels.length} matching label(s) on the pull request (${matchingLabels.join(",")})`)
+    
+    if (matchingLabels.length === 0) {
+        throw new Error("No matching required labels found.");
+    }
 }
-
-
-
 
 
 runAction();
