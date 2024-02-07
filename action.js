377dfe2abb4acc452990a152ccc90bc27ee6cf47
Joakim Sørensen <joasoe@gmail.com>
2024-02-07 14:02:43 +0100

    Make errors prettier (#9)
    * Make errors prettier
    
    * use .log
    
    * Access the messaage directly
    
    * Add fallback to the err

--- action.js
+++ action.js
@@ -7,7 +7,7 @@ function runAction() {
         throw new Error(`GITHUB_EVENT_PATH ${eventPath} does not exist`)
     }
 
-    const eventData = JSON.parse(fs.readFileSync(eventPath, {encoding: 'utf8'}))
+    const eventData = JSON.parse(fs.readFileSync(eventPath, { encoding: 'utf8' }))
 
     if (!eventData.pull_request) {
         throw new Error("This is not a pull request.")
@@ -18,7 +18,7 @@ function runAction() {
     }
 
     const inputLabels = process.env.INPUT_LABELS
-    
+
     if (!inputLabels) {
         throw new Error("No required labels defined for the action.")
     }
@@ -31,11 +31,16 @@ function runAction() {
 
     const matchingLabels = prLabels.filter(label => requiredLabels.has(label))
     console.log(`Found ${matchingLabels.length} matching label(s) on the pull request (${matchingLabels.join(",")})`)
-    
+
     if (matchingLabels.length === 0) {
         throw new Error("No matching required labels found.");
     }
 }
 
+try {
+    runAction();
+} catch (err) {
+    console.log("::error::", err.message || err.toString())
+    process.exitCode = 1
+}
 
-runAction();
